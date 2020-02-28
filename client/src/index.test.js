c225260bf7eca05e8356762dde6149937d3fffe9
arjunalapsapkota <alapzero@gmail.com>
2020-02-27 19:58:22 -0600

    configure jest, enzyme

--- client/src/index.test.js
+++ client/src/index.test.js
@@ -1,8 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 import Enzyme, { shallow } from "enzyme";
 import EnzymeAdapter from "enzyme-adapter-react-16";
-import App from "./app.js";
+//import App from "./app.js";
+class App extends Component {
+  render() {
+    return <h1 className="text-center">Welcome to the first react app</h1>;
+  }
+}
+
 Enzyme.configure({ adapter: new EnzymeAdapter() });
 test("App should render correctly", () => {
   const wrapper = shallow(<App />);
+  expect(wrapper).toMatchSnapshot();
 });
