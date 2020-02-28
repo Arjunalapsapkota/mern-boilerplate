8eda0e523e396673703b413628c56f5c10f83467
arjunalapsapkota <alapzero@gmail.com>
2020-02-28 11:42:37 -0600

    configured for jest coverage

--- server/server.test.js
+++ server/server.test.js
@@ -1,12 +1,12 @@
-const sum = require("../server/server.js");
+// const sum = require("./server.js");
 
-const assert = require("chai").assert;
+// const assert = require("chai").assert;
 
-// test("adds 1 + 2 to equal 3", () => {
-//   expect(sum(1, 2)).toBe(3);
+// // test("adds 1 + 2 to equal 3", () => {
+// //   expect(sum(1, 2)).toBe(3);
+// // });
+// describe("app", () => {
+//   it("app should return hello", () => {
+//     assert.equal(sum(2, 2), 4);
+//   });
 // });
-describe("app", () => {
-  it("app should return hello", () => {
-    assert.equal(sum(2, 2), 4);
-  });
-});
