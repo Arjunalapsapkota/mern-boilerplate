ef1af0dde086075aa86e542b96f1459cf260c179
arjunalapsapkota <alapzero@gmail.com>
2020-02-27 22:27:47 -0600

    Added EsLint and Prettier config

--- client/src/index.js
+++ client/src/index.js
@@ -3,5 +3,4 @@ import ReactDOM from "react-dom";
 import App from "./app";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "index.scss";
-
 ReactDOM.render(<App />, document.getElementById("root"));
